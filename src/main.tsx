eb313b1000d4b0de1a0999fff77344bed17f0608
Alevar <chupick228@gmail.com>
2024-02-21 15:02:30 +0300

    feat: disable tanstack devtools

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import router from "./routes";
 import { RouterProvider } from "react-router-dom";
 import theme from "./theme";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const client = new QueryClient();
 
@@ -15,7 +15,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <QueryClientProvider client={client}>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <RouterProvider router={router} />
-        <ReactQueryDevtools />
+        {/* <ReactQueryDevtools /> */}
       </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>
